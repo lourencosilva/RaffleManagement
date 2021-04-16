2028d4853982555807c7dae8bd27d20ffe195f72
Lourenço Silva Júnior <lourenco.silva72@gmail.com>
2021-04-16 17:45:36 -0300

    Alter default

--- app/controllers/raffles_controller.rb
+++ app/controllers/raffles_controller.rb
@@ -1,9 +1,12 @@
 class RafflesController < ApplicationController
   before_action :set_raffle, only: [:show, :edit, :update, :destroy]
+  # skip_before_action :authenticate_user!, [:new, :create]
 
   # GET /raffles
   def index
     @raffles = Raffle.all
+    # @raffles = current_user.raffles
+
   end
 
   # GET /raffles/1
@@ -12,7 +15,7 @@ class RafflesController < ApplicationController
 
   # GET /raffles/new
   def new
-    @raffle = Raffle.new
+    @raffle = Raffle.new()
   end
 
   # GET /raffles/1/edit
@@ -22,9 +25,10 @@ class RafflesController < ApplicationController
   # POST /raffles
   def create
     @raffle = Raffle.new(raffle_params)
+    @raffle.user = current_user
 
     if @raffle.save
-      redirect_to @raffle, notice: 'Raffle was successfully created.'
+      redirect_to @raffle, notice: "Raffle was successfully created."
     else
       render :new
     end
@@ -33,7 +37,7 @@ class RafflesController < ApplicationController
   # PATCH/PUT /raffles/1
   def update
     if @raffle.update(raffle_params)
-      redirect_to @raffle, notice: 'Raffle was successfully updated.'
+      redirect_to @raffle, notice: "Raffle was successfully updated."
     else
       render :edit
     end
@@ -42,17 +46,18 @@ class RafflesController < ApplicationController
   # DELETE /raffles/1
   def destroy
     @raffle.destroy
-    redirect_to raffles_url, notice: 'Raffle was successfully destroyed.'
+    redirect_to raffles_url, notice: "Raffle was successfully destroyed."
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_raffle
-      @raffle = Raffle.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def raffle_params
-      params.require(:raffle).permit(:title, :description, :probable_draw_at, :start_sale_at, :finishes_sale_at, :drawn_at, :ticket_value, :user_id, :kind_id)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_raffle
+    @raffle = Raffle.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def raffle_params
+    params.require(:raffle).permit(:title, :description, :probable_draw_at, :start_sale_at, :finishes_sale_at, :ticket_value, :kind_id)
+  end
 end
